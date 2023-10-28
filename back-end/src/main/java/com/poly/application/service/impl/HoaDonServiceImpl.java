a3099bd17936ffbec4867898cf1b075b42479d16
phuongdtY <homnayphuonglonglanh@gmail.com>
2023-10-28 23:24:23 +0700

    add hóa đơn thêm tự gencode của mã hóa đơn

--- back-end/src/main/java/com/poly/application/service/impl/HoaDonServiceImpl.java
+++ back-end/src/main/java/com/poly/application/service/impl/HoaDonServiceImpl.java
@@ -1,6 +1,7 @@
 package com.poly.application.service.impl;
 
 import com.poly.application.common.CommonEnum;
+import com.poly.application.common.GenCode;
 import com.poly.application.entity.HoaDon;
 import com.poly.application.entity.HoaDonChiTiet;
 import com.poly.application.entity.TaiKhoan;
@@ -79,6 +80,7 @@ public class HoaDonServiceImpl implements HoaDonService {
             createHoaDon.setTaiKhoan(taiKhoan);
         }
 
+        createHoaDon.setMa(GenCode.generateHoaDonCode());
         HoaDon savedHoaDon = hoaDonRepository.save(createHoaDon);
         return hoaDonMapper.convertHoaDonEntityToHoaDonResponse(savedHoaDon);
     }
