a425c7e071ebb8b1f06a903d31ddbcd881910790
canhdv281 <canhdat28@gmail.com>
2023-10-29 23:46:28 +0700

    update kich co

--- back-end/src/main/java/com/poly/application/repository/KichCoRepository.java
+++ back-end/src/main/java/com/poly/application/repository/KichCoRepository.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @Repository
 public interface KichCoRepository extends JpaRepository<KichCo, Long> {
-    @Query("SELECT kc FROM KichCo kc WHERE kc.trangThai = 'ACTIVE' ORDER BY kc.ngayTao DESC")
+    @Query("SELECT kc FROM KichCo kc WHERE kc.trangThai = 'ACTIVE' ORDER BY kc.kichCo ASC, kc.ngayTao DESC")
     List<KichCo> getKichCoByNgayTaoDESC();
 
     @Query("SELECT obj FROM KichCo obj WHERE (CAST(obj.kichCo AS string) LIKE %:searchText%) AND (:trangThai IS NULL OR obj.trangThai = :trangThai)")
