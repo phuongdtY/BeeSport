963551f9b64ba6272b4e019fc81c8c566137a9aa
phuongdtY <homnayphuonglonglanh@gmail.com>
2023-11-02 00:26:50 +0700

    thêm chi tiết sản phảm vào hóa đơn chit tiết

--- font-end/src/interfaces/ctsp.type.ts
+++ font-end/src/interfaces/ctsp.type.ts
@@ -1,25 +1,70 @@
 export interface DataType {
+  id: number;
   key: string;
   soLuong: number;
   giaTien: number;
-  sanPham: {
-    id: number;
-    ten: string;
-  };
   loaiDe: {
     id: number;
     ten: string;
+    ngayTao: string;
+    ngaySua: string;
+    trangThai: {
+      ten: string;
+      mota: string;
+      mauSac: string;
+    };
   };
   diaHinhSan: {
     id: number;
     ten: string;
+    ngayTao: string;
+    ngaySua: string;
+    trangThai: {
+      ten: string;
+      mota: string;
+      mauSac: string;
+    };
+  };
+  sanPham: {
+    id: number;
+    ma: string;
+    ten: string;
+    ngayTao: string;
+    ngaySua: string;
+    moTa: string;
+    thuongHieu: {
+      id: number;
+      ten: string;
+      ngaySua: string;
+      ngayTao: string;
+    };
+    trangThai: {
+      ten: string;
+      mota: string;
+      mauSac: string;
+    };
   };
   mauSac: {
     id: number;
+    ma: string;
     ten: string;
+    ngayTao: string;
+    ngaySua: string;
+    trangThai: {
+      ten: string;
+      mota: string;
+      mauSac: string;
+    };
   };
   kichCo: {
     id: number;
-    ten: string;
+    kichCo: number;
+    ngayTao: string;
+    ngaySua: string;
+    trangThai: {
+      ten: string;
+      mota: string;
+      mauSac: string;
+    };
   };
 }
