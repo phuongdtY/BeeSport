3a23fc9115dc5ae6c49200b16abf4c094ab25b37
canhdv281 <canhdat28@gmail.com>
2023-11-27 20:00:30 +0700

    fix

--- back-end/src/main/java/com/poly/application/model/response/ThongKeTheoDMYResponse.java
+++ back-end/src/main/java/com/poly/application/model/response/ThongKeTheoDMYResponse.java
@@ -23,6 +23,8 @@ import java.math.BigDecimal;
                                 @ColumnResult(name = "tongSoDonThanhCong", type = Long.class),
                                 @ColumnResult(name = "tongSoDonHuy", type = Long.class),
                                 @ColumnResult(name = "tongSoSanPhamDaBan", type = Long.class),
+                                @ColumnResult(name = "tongSoDonOnline", type = Long.class),
+                                @ColumnResult(name = "tongSoDonTaiQuay", type = Long.class),
                         }
                 )
         }
