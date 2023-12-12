dfd9e7c86925419f87caf1616fe3b3bf02c69037
canhdv281 <canhdat28@gmail.com>
2023-12-13 00:22:20 +0700

    fix

--- back-end/src/main/java/com/poly/application/repository/DonHangRepository.java
+++ back-end/src/main/java/com/poly/application/repository/DonHangRepository.java
@@ -11,10 +11,10 @@ import java.util.List;
 
 @Repository
 public interface DonHangRepository extends JpaRepository<HoaDon, Long> {
-    @Query("SELECT h FROM HoaDon h JOIN h.taiKhoan t WHERE t.id = :taiKhoanId AND (h.trangThaiHoaDon = :trangThai OR :trangThai IS NULL) ORDER BY h.ngaySua DESC")
+    @Query("SELECT h FROM HoaDon h JOIN h.taiKhoan t WHERE t.id = :taiKhoanId AND (h.trangThaiHoaDon = :trangThai OR :trangThai IS NULL) AND h.loaiHoaDon = 'ONLINE' ORDER BY h.ngaySua DESC")
     List<HoaDon> getAllHoaDonCuaTaiKhoan(@Param("taiKhoanId") Long id, @Param("trangThai")CommonEnum.TrangThaiHoaDon trangThaiHoaDon);
 
-    @Query("SELECT COUNT(h) FROM HoaDon h WHERE h.taiKhoan.id = :taiKhoanId AND (h.trangThaiHoaDon = :trangThai OR :trangThai IS NULL)")
+    @Query("SELECT COUNT(h) FROM HoaDon h WHERE h.taiKhoan.id = :taiKhoanId AND (h.trangThaiHoaDon = :trangThai OR :trangThai IS NULL)  AND h.loaiHoaDon = 'ONLINE'")
     Long countSoHoaDon(@Param("taiKhoanId") Long taiKhoanId, @Param("trangThai") CommonEnum.TrangThaiHoaDon trangThaiHoaDon);
 
 }
